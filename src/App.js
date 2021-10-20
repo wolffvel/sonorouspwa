a7fe0764292fe2488ddfa8356bbce03b5ae0ea47
Pablo Wolff <pabloawolff@gmail.com>
2021-10-20 09:38:27 -0500

    Add assets to repo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.png";
+import track from "./track.mp3";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <div className="container">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <audio controls>
+          <source src={track} />
+        </audio>
+      </div>
     </div>
   );
 }
